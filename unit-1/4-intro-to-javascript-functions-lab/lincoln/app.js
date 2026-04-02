1cedf268a85c7ec5fd0bed976051a69029a77a9c
Lincoln <lklim050@gmail.com>
2026-04-02 22:20:47 +0800

    completed exercise 2

--- unit-1/4-intro-to-javascript-functions-lab/lincoln/app.js
+++ unit-1/4-intro-to-javascript-functions-lab/lincoln/app.js
@@ -1,21 +1,45 @@
+// // Unit 1 - 4 Intro to JavaScript Functions Lab Exercise
+// /*
+// Exercise 1: maxOfTwoNumbers()
+
+// In this exercise, create a function named maxOfTwoNumbers.
+// It should take two numbers as inputs and return the larger number.
+// If they're equal, return either one.
+
+// Exercise 1 has been completed for you:
+// */
+
+// const maxOfTwoNumbers = (x, y) => {
+//   if (x >= y) {
+//     return x;
+//   } else {
+//     return y;
+//   }
+// };
+
+// console.log("Exercise 1 Result:", maxOfTwoNumbers(3, 9));
+// //numbers 3 and 9 passed into x and y respectively of the function maxOfTwoNumbers
+// // result expected is to be 9 since the purpose of the function is to check and return the larger number
+
 /*
-Exercise 1: maxOfTwoNumbers()
+Exercise 2: isAdult()
 
-In this exercise, create a function named maxOfTwoNumbers. 
-It should take two numbers as inputs and return the larger number. 
-If they're equal, return either one.
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
 
-Exercise 1 has been completed for you:
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
 */
 
-const maxOfTwoNumbers = (x, y) => {
-  if (x >= y) {
-    return x;
+const isAdult = (x) => {
+  const adultAge = 18;
+  if (x >= adultAge) {
+    return "Adult";
   } else {
-    return y;
+    return "Minor";
   }
 };
 
-console.log("Exercise 1 Result:", maxOfTwoNumbers(3, 9));
-//numbers 3 and 9 passed into x and y respectively of the function maxOfTwoNumbers
-// result expected is to be 9 since the purpose of the function is to check and return the larger number
+console.log("Exercise 2 Result:", isAdult(21));
+// Expect result to be Adult
