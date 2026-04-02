544e57bd718906665425d9951e036d1e4854c441
Lincoln <lklim050@gmail.com>
2026-04-02 23:26:38 +0800

    exer 6 of unit 1-4 done

--- unit-1/4-intro-to-javascript-functions-lab/lincoln/app.js
+++ unit-1/4-intro-to-javascript-functions-lab/lincoln/app.js
@@ -44,23 +44,58 @@
 // console.log("Exercise 2 Result:", isAdult(21));
 // // Expect result to be Adult
 
+// /*
+// Exercise 3: isCharAVowel()
+
+// Write a function named isCharAVowel that takes a single character as
+// an argument. It should return true if the character is a vowel and
+// false otherwise. For the purposes of this exercise, the character
+// y should not be considered a vowel.
+
+// Example: isCharAVowel('a') should return true.
+
+// Complete the exercise in the space below:
+// */
+// // Let me try ternary expression
+// const isCharAVowel = (char) =>
+//   char === "a" || char === "e" || char === "i" || char === "o" || char === "u"
+//     ? "Vowel"
+//     : "Not a vowel";
+
+// console.log("Exercise 3 Result:", isCharAVowel("a"));
+// // Expected result would be vowel
+
+// /*
+// Exercise 4: generateEmail()
+
+// Create a function named generateEmail. It should take two strings:
+// a name and a domain. It should return a simple email address.
+
+// Example: generateEmail('johnsmith', 'example.com')
+// should return 'johnsmith@example.com'.
+
+// Complete the exercise in the space below:
+// */
+// const generateEmail = (name, domain) => {
+//   return name + "@" + domain;
+// };
+// console.log("Exercise 4 Result:", generateEmail("johnsmith", "example.com"));
+// // Expected result would be johnsmith@example.com
+
 /*
-Exercise 3: isCharAVowel()
+Exercise 5: greetUser()
 
-Write a function named isCharAVowel that takes a single character as 
-an argument. It should return true if the character is a vowel and 
-false otherwise. For the purposes of this exercise, the character 
-y should not be considered a vowel.
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
 
-Example: isCharAVowel('a') should return true.
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
 
 Complete the exercise in the space below:
 */
-// Let me try ternary expression
-const isCharAVowel = (char) =>
-  char === "a" || char === "e" || char === "i" || char === "o" || char === "u"
-    ? "Vowel"
-    : "Not A Vowel";
 
-console.log("Exercise 3 Result:", isCharAVowel("a"));
-// Expected result would be vowel
+// this is template literal
+const greetUser = (name, timeOfDay) => `Good ${timeOfDay}, ${name}!`;
+
+console.log("Exercise 5 Result:", greetUser("Sam", "morning"));
+// Expected result would be Good morning, Sam!
