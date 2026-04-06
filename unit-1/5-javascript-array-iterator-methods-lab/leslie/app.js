6c3170e9c97b4814882ae91131e6e8674b0534a0
leslietan1981 <leslietan.yw1981@gmail.com>
2026-04-06 16:25:36 +0800

    Update app.js with Level Up Exercise 2

--- unit-1/5-javascript-array-iterator-methods-lab/leslie/app.js
+++ unit-1/5-javascript-array-iterator-methods-lab/leslie/app.js
@@ -398,3 +398,46 @@ totalYearsLived = inventors.reduce(
 // Check your work:
 console.log("Level Up 1 my result: ", totalYearsLived);
 console.log("Level Up 1 correct result: ", 861);
+
+/*
+Level Up exercise 2: Array.prototype.reduce()
+
+Tallying travel methods using Array.prototype.reduce(). 
+
+Count the number of times each travel method appears in the 'travelMethods'
+array.
+
+- The resulting object should have keys as the travel methods 
+  ('car', 'truck', 'bike', etc.) and values as their respective counts.
+- Store this object in the variable 'travelMethodCounts'.
+
+Hints:
+- Inside the reduce function, check if the travel method already exists as a key
+  in your accumulator object. If it does, increment its count. If not, add it 
+  to the object and give it a value of 1.
+- Since you want to return an object, be sure to pass an empty {} for the 
+  initial value of the "accumulator".
+*/
+
+let travelMethodCounts = {};
+
+// Complete the exercise in the space below:
+
+travelMethodCounts = travelMethods.reduce((accumul, travelMethod) => {
+  if (travelMethod in accumul) {
+    accumul[travelMethod] += 1;
+  } else {
+    accumul[travelMethod] = 1;
+  }
+  return accumul;
+}, {});
+
+// Check your work:
+console.log("Level Up 2 my result: ", travelMethodCounts);
+console.log("Level Up 2 correct result: ", {
+  car: 5,
+  truck: 3,
+  bike: 2,
+  walk: 2,
+  van: 2,
+});
