34e956d5fa4b3aad35adcf3f4ca889ea174f7429
leslietan1981 <leslietan.yw1981@gmail.com>
2026-04-21 16:23:22 +0800

    Remove commented lines that were used for reference

--- unit-2/1-react-components-lab/leslie/src/components/WeatherForecast.jsx
+++ unit-2/1-react-components-lab/leslie/src/components/WeatherForecast.jsx
@@ -20,12 +20,3 @@ const WeatherForecast = (props) => {
 };
 
 export default WeatherForecast;
-
-{
-  /* <div className="weather">
-  <h2>Day of the Week</h2>
-  <img src="" alt="" />
-  <p><span>conditions: </span>current weather conditions</p>
-  <p><span>time: </span>time of day</p>
-</div> */
-}
