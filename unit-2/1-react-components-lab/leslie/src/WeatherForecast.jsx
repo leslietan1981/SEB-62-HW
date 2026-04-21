70f1f510fa51cc96a879146fbe85ef25957a3fda
leslietan1981 <leslietan.yw1981@gmail.com>
2026-04-21 16:00:26 +0800

    Create WeatherForecast component and add WeatherForecast.css

---
+++ unit-2/1-react-components-lab/leslie/src/WeatherForecast.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+const WeatherForecast = (props) => {
+  return (
+    <div className="weather">
+      <h2>{props.day}</h2>
+      <img src={props.img} alt={props.imgAlt} />
+      <p>
+        <span>conditions: </span>
+        {props.conditions}
+      </p>
+      <p>
+        <span>time: </span>
+        {props.time}
+      </p>
+    </div>
+  );
+};
+
+export default WeatherForecast;
+
+{
+  /* <div className="weather">
+  <h2>Day of the Week</h2>
+  <img src="" alt="" />
+  <p><span>conditions: </span>current weather conditions</p>
+  <p><span>time: </span>time of day</p>
+</div> */
+}
