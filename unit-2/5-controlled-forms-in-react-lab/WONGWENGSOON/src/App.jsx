ecd8f7d0fb227a3f53f6c6389d10a78ebd6fb70e
Genius-farmer <76843221+Genius-farmer@users.noreply.github.com>
2026-04-27 23:19:20 +0800

    homework

---
+++ unit-2/5-controlled-forms-in-react-lab/WONGWENGSOON/src/App.jsx
@@ -0,0 +1,52 @@
+// src/App.jsx
+import IngredientList from "./components/IngredientList/IngredientList";
+import BurgerStack from "./components/BurgerStack/BurgerStack";
+import "./App.css";
+import { useState } from "react";
+
+const App = () => {
+  const availableIngredients = [
+    { name: "Kaiser Bun", color: "saddlebrown" },
+    { name: "Sesame Bun", color: "sandybrown" },
+    { name: "Gluten Free Bun", color: "peru" },
+    { name: "Lettuce Wrap", color: "olivedrab" },
+    { name: "Beef Patty", color: "#3F250B" },
+    { name: "Soy Patty", color: "#3F250B" },
+    { name: "Black Bean Patty", color: "#3F250B" },
+    { name: "Chicken Patty", color: "burlywood" },
+    { name: "Lettuce", color: "lawngreen" },
+    { name: "Tomato", color: "tomato" },
+    { name: "Bacon", color: "maroon" },
+    { name: "Onion", color: "lightyellow" },
+    { name: "Cheddar Cheese", color: "#FDE18B" },
+    { name: "Swiss Cheese", color: "#F1E1A8" },
+  ];
+
+  const [stack, setStack] = useState([]);
+
+  const addToBurger = (ingredient) => {
+    setStack((prevStack) => [...prevStack, ingredient]);
+  };
+
+  const removeFromBurger = (indexToRemove) => {
+    setStack((prevStack) =>
+      prevStack.filter((_, index) => index !== indexToRemove),
+    );
+  };
+
+  return (
+    <main>
+      <h1>Burger Stacker</h1>
+      <section>
+        <IngredientList
+          ingredients={availableIngredients}
+          addToBurger={addToBurger}
+        />
+        <BurgerStack ingredients={stack} removeFromBurger={removeFromBurger} />
+        {/* List & Stack components */}
+      </section>
+    </main>
+  );
+};
+
+export default App;
